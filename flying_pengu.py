eac98b80fb2635021e1aeb7e1d09af00244f4212
DannyNugget <dfb.buettner@gmail.com>
2025-03-20 10:41:09 +0100

    added start screen and difficulties

--- flying_pengu.py
+++ flying_pengu.py
@@ -4,6 +4,10 @@ import math
 import random
 import numpy as np
 
+# Global variable to start the game and choose the difficulty
+start_game = True
+difficulty = 1
+
 # help function
 def print2file(output_string):
     with open('output.txt', 'a') as f:
@@ -62,6 +66,8 @@ def main(stdscr):
     curses.curs_set(0)
     stdscr.nodelay(True)  # This allows getch() to be non-blocking
 
+    global start_game, difficulty
+
     total_height, total_width = 30, 150
 
     # Create a 2D NumPy array for the screen filled with spaces.
@@ -115,6 +121,42 @@ def main(stdscr):
     # going to track the upper left corner of the penguin
     y = y_start
     y_p = 0
+
+    if start_game:
+        # Create the start screen and wait for the user to choose a difficulty.
+        start_game = False
+        start_screen = create_start_screen(total_height, total_width)
+        stdscr.clear()
+        for row in range(total_height):
+            for col in range(total_width):
+                stdscr.addch(row, col, start_screen[row, col])
+        stdscr.refresh()
+        while True:
+            key = stdscr.getch()
+            if key == 49:
+                difficulty = 1
+                break
+            elif key == 50:
+                difficulty = 2
+                break
+            elif key == 51:
+                difficulty = 3
+                break
+            elif key == 27:
+                curses.endwin()  # reset the terminal
+                return           # exit main() function
+            
+    if difficulty == 1:
+        fps = 30
+        opening_height = 12
+    elif difficulty == 2:
+        fps = 40
+        opening_height = 10
+    elif difficulty == 3:
+        fps = 45
+        opening_height = 10
+        wall_distance = 32 + wallwidth
+
 
     while True:
 
@@ -292,6 +334,33 @@ def check_collision(screen_array, penguin_art):
             if screen_array[r, c] == '|' and penguin_art[r, c] != ' ':
                 return True
     return False
+
+def create_start_screen(height, width):
+    """ Creates the start screen with options to choose the difficulty.
+    Args:
+        height (int): The height of the screen.
+        width (int): The width of the screen.
+    Returns:
+        np.ndarray: A 2D array representing the pause screen.
+    """
+    center_width = width // 2
+    center_height = height // 2
+    start_screen = np.full((height, width), ' ', dtype=str)
+
+    str_name = "Pengu Fly"
+    str_press = "Chosse the difficulty by pressing the number:"
+    str_easy = "1 - Easy"
+    str_medium = "2 - Medium"
+    str_hard = "3 - Hard"
+    str_esc = "Press ESC to quit the game"
+    start_screen[center_height - 5, center_width - math.floor(len(str_name)/2):center_width + math.ceil(len(str_name)/2)] = list(str_name)
+    start_screen[center_height - 2, center_width - math.floor(len(str_press)/2):center_width + math.ceil(len(str_press)/2)] = list(str_press)
+    start_screen[center_height, center_width - math.floor(len(str_easy)/2):center_width + math.ceil(len(str_easy)/2)] = list(str_easy)
+    start_screen[center_height + 1, center_width - math.floor(len(str_medium)/2):center_width + math.ceil(len(str_medium)/2)] = list(str_medium)
+    start_screen[center_height + 2, center_width - math.floor(len(str_hard)/2):center_width + math.ceil(len(str_hard)/2)] = list(str_hard)
+    start_screen[center_height + 3, center_width - math.floor(len(str_esc)/2):center_width + math.ceil(len(str_esc)/2)] = list(str_esc)
+    return start_screen
+
 
 def create_pause_screen(height, width, score):
     """ Creates the pause screen with a message and the current score.
